6d5a79c45331ba1889f640063df4ff618d59d67a
BolaWagdy <bolawagdy05@gmail.com>
2024-08-08 22:51:53 +0000

    Terraform

---
+++ terraform/terraform-ec2.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "terraform_sg" {
+  name_prefix = "terraform-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-04a81a99f5ec58529" 
+  instance_type = "t2.micro"
+
+  key_name        = "vockey"
+  security_groups = [aws_security_group.terraform_sg.name]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
